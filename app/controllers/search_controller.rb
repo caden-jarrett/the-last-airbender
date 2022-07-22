113e298f1004fe89914c4acbc74ea8b288c08941
Caden Jarrett <caden.jarrett99@gmail.com>
2022-07-22 10:57:38 -0600

    Docs: unable to find count of affiliated members of a nation

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
     def index
         @members = MemberFacade.all_members_from(params[:nation])
+        @memberscount = MemberFacade.count_all_members(params[:nation])
     end
 end
--- app/facades/member_facade.rb
+++ app/facades/member_facade.rb
@@ -2,4 +2,8 @@ class MemberFacade
     def self.all_members_from(nation)
         MemberService.members_from(nation).map { |data| Member.new(data)}
     end
+
+    def self.count_all_members(nation)
+        MemberService.count_all_members
+    end
 end
